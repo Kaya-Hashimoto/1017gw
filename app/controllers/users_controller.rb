484877d9ba42d2451831d5d4fd78cc91f1ce91bb
kaya <kaaya.xx.x@gmail.com>
2023-10-17 18:11:34 +0900

    ログインシステム・ Bootstrap実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,15 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
       # 成功
+      redirect_to user_path(@user.id)
     else
       # 失敗
       render :new
@@ -17,6 +22,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                :password_confirmation)
   end
 end
